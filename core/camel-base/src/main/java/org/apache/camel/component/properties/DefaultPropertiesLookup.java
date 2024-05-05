c5137bfd5e53a28a7943cf0d4fe8caf53f6ff641
Anton Hrabianets <anton.hrabianets@gmail.com>
2024-05-05 09:53:33 +0200

    DefaultPropertiesLookup cleanup

--- core/camel-base/src/main/java/org/apache/camel/component/properties/DefaultPropertiesLookup.java
+++ core/camel-base/src/main/java/org/apache/camel/component/properties/DefaultPropertiesLookup.java
@@ -16,14 +16,17 @@
  */
 package org.apache.camel.component.properties;
 
-import java.util.Properties;
-
+import lombok.SneakyThrows;
 import org.apache.camel.NoTypeConversionAvailableException;
 import org.apache.camel.PropertiesLookupListener;
 import org.apache.camel.RuntimeCamelException;
 import org.apache.camel.spi.LoadablePropertiesSource;
 import org.apache.camel.spi.PropertiesSource;
 import org.apache.camel.util.OrderedLocationProperties;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
 
 /**
  * Default {@link PropertiesLookup} which lookup properties from a {@link java.util.Properties} with all existing
@@ -47,76 +50,104 @@ public class DefaultPropertiesLookup implements PropertiesLookup {
     }
 
     private String doLookup(String name, String defaultValue) throws NoTypeConversionAvailableException {
-        String answer = null;
-
         // local takes precedence
         Properties local = component.getLocalProperties();
-        if (local != null) {
-            // use get as the value can potentially be stored as a non string value
-            Object value = local.get(name);
-            if (value != null) {
-                answer = component.getCamelContext().getTypeConverter().mandatoryConvertTo(String.class, value);
-                String loc = location(local, name, "LocalProperties");
-                String localDefaultValue = null;
-                if (local instanceof OrderedLocationProperties) {
-                    Object val = ((OrderedLocationProperties) local).getDefaultValue(name);
-                    if (val != null) {
-                        localDefaultValue
-                                = component.getCamelContext().getTypeConverter().mandatoryConvertTo(String.class, val);
-                    }
-                }
-                onLookup(name, answer, localDefaultValue, loc);
-            }
-        }
+        return findInLocalProperties(local, name)
+                .or(() -> findInOverrideProperties(component.getOverrideProperties(), local, name, defaultValue))
+                .or(() -> tryToFindInPropertiesSources(component.getPropertiesSources(), name, defaultValue))
+                .or(() -> findInInitialProperties(component.getInitialProperties(), local, name, defaultValue))
+                .orElse(null);
+    }
 
-        // override takes precedence
-        if (answer == null && component.getOverrideProperties() != null) {
-            // use get as the value can potentially be stored as a non string value
-            Object value = component.getOverrideProperties().get(name);
-            if (value != null) {
-                answer = component.getCamelContext().getTypeConverter().mandatoryConvertTo(String.class, value);
-                String loc = location(local, name, "OverrideProperties");
-                onLookup(name, answer, defaultValue, loc);
-            }
-        }
-        if (answer == null) {
-            // try till first found source
-            for (PropertiesSource ps : component.getPropertiesSources()) {
-                answer = ps.getProperty(name, defaultValue);
-                if (answer != null) {
-                    String source = ps.getName();
-                    if (ps instanceof ClasspathPropertiesSource) {
-                        source = "classpath:" + ((LocationPropertiesSource) ps).getLocation().getPath();
-                    } else if (ps instanceof FilePropertiesSource) {
-                        source = "file:" + ((LocationPropertiesSource) ps).getLocation().getPath();
-                    } else if (ps instanceof RefPropertiesSource) {
-                        source = "ref:" + ((LocationPropertiesSource) ps).getLocation().getPath();
-                    } else if (ps instanceof LocationPropertiesSource) {
-                        source = ((LocationPropertiesSource) ps).getLocation().getPath();
-                    } else if (ps instanceof LoadablePropertiesSource) {
-                        Properties prop = ((LoadablePropertiesSource) ps).loadProperties();
-                        if (prop instanceof OrderedLocationProperties) {
-                            OrderedLocationProperties olp = (OrderedLocationProperties) prop;
-                            source = olp.getLocation(name);
-                        }
-                    }
-                    onLookup(name, answer, defaultValue, source);
-                    break;
-                }
-            }
-        }
-        // initial properties are last
-        if (answer == null && component.getInitialProperties() != null) {
-            // use get as the value can potentially be stored as a non string value
+    @SneakyThrows(NoTypeConversionAvailableException.class)
+    private Optional<String> findInInitialProperties(Properties initialProperties, Properties local, String name, String defaultValue) {
+        // use get as the value can potentially be stored as a non string value
+        if (initialProperties != null) {
+            String answer = null;
             Object value = component.getInitialProperties().get(name);
             if (value != null) {
                 answer = component.getCamelContext().getTypeConverter().mandatoryConvertTo(String.class, value);
                 String loc = location(local, name, "InitialProperties");
                 onLookup(name, answer, defaultValue, loc);
             }
+            return Optional.ofNullable(answer);
         }
+        return Optional.empty();
+    }
 
-        return answer;
+    @SneakyThrows(NoTypeConversionAvailableException.class)
+    private Optional<String> findInOverrideProperties(Properties overrideProperties, Properties local, String name, String defaultValue) {
+        String answer = null;
+        if (overrideProperties != null) {
+            // use get as the value can potentially be stored as a non string value
+            Object value = overrideProperties.get(name);
+            if (value != null) {
+                answer = component.getCamelContext().getTypeConverter().mandatoryConvertTo(String.class, value);
+                String loc = location(local, name, "OverrideProperties");
+                onLookup(name, answer, defaultValue, loc);
+            }
+        }
+        return Optional.ofNullable(answer);
+    }
+
+    private Optional<String> findInLocalProperties(Properties local, String name) throws NoTypeConversionAvailableException {
+        String answer = null;
+        if (local != null) {
+            // use get as the value can potentially be stored as a non string value
+            Object value = local.get(name);
+            if (value != null) {
+                answer = component.getCamelContext().getTypeConverter().mandatoryConvertTo(String.class, value);
+                String loc = location(local, name, "LocalProperties");
+                String localDefaultValue = getLocalDefaultValue(local, name);
+                onLookup(name, answer, localDefaultValue, loc);
+            }
+        }
+        return Optional.ofNullable(answer);
+    }
+
+    private String getLocalDefaultValue(Properties local, String name) throws NoTypeConversionAvailableException {
+        String localDefaultValue = null;
+        if (local instanceof OrderedLocationProperties) {
+            Object val = ((OrderedLocationProperties) local).getDefaultValue(name);
+            if (val != null) {
+                localDefaultValue
+                        = component.getCamelContext().getTypeConverter().mandatoryConvertTo(String.class, val);
+            }
+        }
+        return localDefaultValue;
+    }
+
+    private Optional<String> tryToFindInPropertiesSources(List<PropertiesSource> propertiesSources, String name, String defaultValue) {
+        String answer;
+        // try till first found source
+        for (PropertiesSource ps : propertiesSources) {
+            answer = ps.getProperty(name, defaultValue);
+            if (answer != null) {
+                String source = getSource(ps, name);
+                onLookup(name, answer, defaultValue, source);
+                return Optional.of(answer);
+            }
+        }
+        return Optional.empty();
+    }
+
+    private String getSource(PropertiesSource ps, String name) {
+        String source = ps.getName();
+        if (ps instanceof ClasspathPropertiesSource) {
+            source = "classpath:" + ((LocationPropertiesSource) ps).getLocation().getPath();
+        } else if (ps instanceof FilePropertiesSource) {
+            source = "file:" + ((LocationPropertiesSource) ps).getLocation().getPath();
+        } else if (ps instanceof RefPropertiesSource) {
+            source = "ref:" + ((LocationPropertiesSource) ps).getLocation().getPath();
+        } else if (ps instanceof LocationPropertiesSource) {
+            source = ((LocationPropertiesSource) ps).getLocation().getPath();
+        } else if (ps instanceof LoadablePropertiesSource) {
+            Properties prop = ((LoadablePropertiesSource) ps).loadProperties();
+            if (prop instanceof OrderedLocationProperties olp) {
+                source = olp.getLocation(name);
+            }
+        }
+        return source;
     }
 
     private void onLookup(String name, String value, String defaultValue, String source) {
